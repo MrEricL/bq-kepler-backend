e1104c269c6fa453c5b2d5e4321d781af63607c4
MrEricL <eric@Erics-MacBook-Air.local>
2019-02-20 09:30:52 -0500

    fixed the numb of info given

--- server/controller/query.js
+++ server/controller/query.js
@@ -2,7 +2,7 @@ const {syncQuery, asyncQuery} = require("../model/basebq.js")
 
 
 export const getAll = (req, res) => {
-	const query = "SELECT * FROM `rf-erictest.testdata.testtable`"
+	const query = "SELECT * FROM `rf-erictest.testdata.testtable` LIMIT 3"
 	asyncQuery(query)
     .then(resp => res.send(resp));
 }
